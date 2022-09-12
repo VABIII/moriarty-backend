973f17d3ad988206cf10a3f88e9648ed65a7e149
VABIII <tbengtson82@gmail.com>
2022-09-12 18:43:13 -0400

    added database migrations

---
+++ moriarty-backend/data/migrations/20220912220748_books.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', tbl => {
+            tbl.increments('userId')
+        })
+        .createTable('books', tbl => {
+            tbl.increments('bookId')
+            tbl.string('title', 255).unique().notNullable()
+            tbl.boolean('finished').notNullable()
+            tbl.integer('userId', 255)
+                .unsigned()
+                .notNullable()
+                .references('userId')
+                .inTable('users')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE')
+        })
+
+};
+
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('books')
+        .dropTableIfExists('users')
+};
--- moriarty-backend/index.js
+++ moriarty-backend/index.js
@@ -1,12 +1,3 @@
-// require('dotenv').config();
-// const server = require('./api/server');
-//
-//
-//
-// const port = process.env.PORT || 4000;
-//
-// server.listen(port, () => console.log(`listening on port ${port}`));
-
 require('dotenv').config();
 
 const server = require('./api/server');
